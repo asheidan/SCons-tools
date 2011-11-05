3449e736dd68ad58e69e5dfd51b7c27e1ff60fd1
Emil Eriksson <asheidan@gmail.com>
2011-11-05 01:55:21 +0100

    Changed cutest to use Pseudo-Builder instead of scanner
    	* Makes compiling large projects possible faster
    	  as it doesn't scan every file on every compile
    	  but only when the test suite is generated.

--- cutest.py
+++ cutest.py
@@ -5,7 +5,6 @@ from SCons.Scanner import Scanner
 def generate(env,**kwargs):
 	setup_environment(env)
 	add_builders(env)
-	add_scanners(env)
 
 
 def exists(env):
@@ -13,16 +12,6 @@ def exists(env):
 
 def setup_environment(env):
 	env.AppendUnique(TEST_CASES=[])
-
-def add_scanners(env):
-	test_case_re = re.compile(r'^\s*void (Test[^\(\s]+)',re.M)
-	def testfile_scan(node, env, path, arg=None):
-		print "scanning %s ..."%(str(node))
-		contents = node.get_text_contents()
-		test_cases = test_case_re.findall(contents)
-		env.Append(TEST_CASES=test_cases)
-		return []
-	env.Append(SCANNERS=Scanner(function=testfile_scan,skeys=['.c']))
 
 def add_builders(env):
 	from string import Template
@@ -53,22 +42,29 @@ int main(void)
 	return 0;
 }
 """)
-	def generate_all_tests(source, target, env, for_signature):
-		# print "generate tests %s %s"%(str(target[0]),str(source))
+	def GenerateAllTests(env, target, source, test_case = re.compile(r'^\s*void (Test[^\(\s]+)',re.M)):
+		if not isinstance(target,list):
+			target = [target]
+		print ":: Generate tests %s < %s"%(str(target[0]),' '.join([str(s) for s in source]))
 		assert(len(source)>=1)
 		assert(len(target)<=1)
+		if env.GetOption('clean'):
+			return ''
 		if(target == None):
 			target[0] = 'AllTests.c'
-		source_files = [str(f) for f in source]
+		TEST_CASES = []
+		for node in source:
+			print "\tscanning %s ..."%(str(node))
+			contents = node.get_text_contents()
+			test_cases = test_case.findall(contents)
+			TEST_CASES.extend(test_cases)
 		f = open(str(target[0]),'w')
 		f.write(template.substitute(
-			EXTTESTS='\n'.join(["extern void %s(CuTest*);"%(case) for case in env['TEST_CASES']]),
-			ADDTESTS='\n'.join(["\tSUITE_ADD_TEST(suite, %s);"%(case) for case in env['TEST_CASES']])))
+			EXTTESTS='\n'.join(["extern void %s(CuTest*);"%(case) for case in TEST_CASES]),
+			ADDTESTS='\n'.join(["\tSUITE_ADD_TEST(suite, %s);"%(case) for case in TEST_CASES])))
 		f.flush()
 		f.close()
-		return ''
+		return target[0]
 		
-	env.Append(BUILDERS={
-		'GenerateAllTests': Builder(
-			generator=generate_all_tests)})
+	env.AddMethod(GenerateAllTests)
 		
